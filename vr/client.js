37f3ce850e5e5c734405eb4589e25fc4637ea948
ArseniyKotov <kotov.a.arseniy@gmail.com>
2017-06-03 13:12:02 -0700

    added raycaster

--- vr/client.js
+++ vr/client.js
@@ -4,11 +4,14 @@
 
 // Auto-generated content.
 import { VRInstance } from 'react-vr-web';
+import * as SimpleRaycaster from "simple-raycaster";
 
 function init(bundle, parent, options) {
   const vr = new VRInstance(bundle, 'VRBasics', parent, {
-    // Add custom options here
-    // enableHotReload: true,
+   raycasters: [
+      SimpleRaycaster // Add SimpleRaycaster to the options 
+    ],
+    cursorVisibility: "auto", // Add cursorVisibility 
     ...options,
   });
   vr.render = function () {
